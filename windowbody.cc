f7de276b65c9bcc818cc2225876a3139cf7fda00
Aniket Deole <aniket.deole@gmail.com>
2013-11-20 22:44:23 +0530

    Created Paned Layout in mainWindow

--- windowbody.cc
+++ windowbody.cc
@@ -1,26 +1,60 @@
 #include <gtkmm/box.h>
 #include <gtkmm/widget.h>
+#include <gtkmm/stylecontext.h>
+#include <gtkmm/cssprovider.h>
+#include <gtkmm/styleprovider.h>
 
 #include "windowbody.hh"
+
+void addCss (Gtk::Widget* widget, std::string cssClass, std::string css) {
+	Glib::RefPtr<Gtk::StyleContext> context;
+	context = widget->get_style_context ();
+	widget->set_name (cssClass);
+	Glib::RefPtr<Gtk::CssProvider> provider = Gtk::CssProvider::create ();
+
+	provider->load_from_data (css);
+	context->add_provider (provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	context->add_class (cssClass);
+}
+
 
 
 WindowBody::WindowBody (bool homogeneous, int spacing, Gtk::PackOptions options, int padding) {
 
-  set_orientation (Gtk::ORIENTATION_VERTICAL);
+	set_orientation (Gtk::ORIENTATION_VERTICAL);
 
-  add (mainToolbar);
+	add (mainToolbar);
 
-// add (leftPane);
-// add (midPane);
-// add (rightPane);
-/*
-    Gtk::Box* body = Gtk::manage(new Gtk::Box());
-    Gtk::Widget* hpaned = Gtk::manage (new Gtk::Paned ());
-    Gtk::EventBox* frame1 = Gtk::manage (new Gtk::EventBox ());
+    Gtk::Box* body = Gtk::manage (new Gtk::Box (Gtk::ORIENTATION_HORIZONTAL, 0));
 
-    add (body);
-*/
-  show_all ();
+    Gtk::Paned* paneOne = Gtk::manage (new Gtk::Paned (Gtk::ORIENTATION_HORIZONTAL));
+    addCss (paneOne, "paneOne", ".paneOne{ -GtkPaned-handle-size: 1px;}");
+
+    Gtk::EventBox* leftPaneEventBox = Gtk::manage (new Gtk::EventBox ());
+    leftPaneEventBox->set_size_request (200, -1);
+    paneOne->pack1 (*leftPaneEventBox, false, false);
+    addCss (leftPaneEventBox, "leftPaneEventBox", ".leftPaneEventBox { background-color: #34393D;}");
+
+    Gtk::Box* rightFrameOfPaneOne = Gtk::manage (new Gtk::Box (Gtk::ORIENTATION_HORIZONTAL, 0));
+    Gtk::Paned* paneTwo = Gtk::manage (new Gtk::Paned (Gtk::ORIENTATION_HORIZONTAL));
+
+    Gtk::EventBox* middlePaneEventBox = Gtk::manage (new Gtk::EventBox ());
+    middlePaneEventBox->set_size_request (300, -1);
+    paneTwo->pack1 (*middlePaneEventBox, false, false);
+    addCss (middlePaneEventBox, "middlePaneEventBox", ".middlePaneEventBox { background-color: white;}");
+
+    Gtk::EventBox* rightPaneEventBox = Gtk::manage (new Gtk::EventBox ());
+    paneTwo->pack2 (*rightPaneEventBox, true, false);
+    addCss (rightPaneEventBox, "rightPaneEventBox", ".rightPaneEventBox { background-color: #DDD;}");
+
+    rightFrameOfPaneOne->pack_start (*paneTwo, true, true, 0);
+
+    paneOne->pack2 (*rightFrameOfPaneOne, true, false);
+
+    body->pack_start (*paneOne, true, true, 0);
+
+    pack_end (*body, Gtk::PACK_EXPAND_WIDGET, 0);
+  	show_all ();
 }
 
 WindowBody::~WindowBody () {
